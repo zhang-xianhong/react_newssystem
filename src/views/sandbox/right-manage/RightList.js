3e24ce1bce73ff078edb81229c75ef7b60ddbad4
zhangxianhong <2827724252@qq.com>
2022-08-01 23:23:27 +0800

    添加权限

--- src/views/sandbox/right-manage/RightList.js
+++ src/views/sandbox/right-manage/RightList.js
@@ -1,6 +1,14 @@
-import React from 'react'
+import React, { useState, useEffect } from 'react';
 
+import axios from 'axios';
 export default function RightList() {
+  const [dataSource, setDataSource] = useState([]);
+  useEffect(() => {
+    axios.get('http://localhost:5000/rights').then(res => {
+      console.log('Right', res.data);
+      setDataSource(res.data);
+    })
+  }, []);
   return (
     <div>RightList</div>
   )
